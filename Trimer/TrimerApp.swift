fae694da46117b9d262a3172dcc6f6f484b2bb78
Jerome Chan <eviltofu@gmail.com>
2021-12-18 08:54:59 +0800

    Initial Commit

---
+++ Trimer/TrimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrimerApp.swift
+//  Trimer
+//
+//  Created by Jerome Chan on 18/12/21.
+//
+
+import SwiftUI
+
+@main
+struct TrimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
